91706f8ab4ec584e9182c783615f622b1e8ab9a1
nischalashashidar <nischala.shashidhar@cognizant.com>
2017-03-31 17:55:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function(req, res){
     res.send("Hello World 31stMar !!!");
 })
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function() {
   console.log('Example app listening on port 3000!')
 })
