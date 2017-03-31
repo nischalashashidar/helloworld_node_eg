668a4cd87592819f087431100d7393fdeb1901af
Nischala <nischala.shashidhar@cognizant.com>
2017-03-31 17:29:29 +0530

    test file changes

--- test/server.test.js
+++ test/server.test.js
@@ -4,14 +4,14 @@ var should = require("should");
 var app = require('../server.js');
 // var expect = require('expect');
 
-var server = supertest.agent("http://localhost:8080");
+var server = supertest.agent("http://localhost:3000");
 
 describe('Tasks CRUD integration testing', function () {
 
-	describe('Get all Tasks', function () {
-        it('Should get status equal success ', function (done) {
+	describe('Get the message', function () {
+        it('Should get message successfully ', function (done) {
 			server
-				.get("/index.htm")
+				.get("/")
 				.end(function (err, results) {
 				 // console.log(results);
 					results.status.should.equal(200);
